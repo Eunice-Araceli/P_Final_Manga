021da271abc8fc5548b4b9bd291aa248d637e459
Eunice-Araceli <eunice.reyes@uabc.edu.mx>
2025-05-21 19:43:28 -0700

    WebScrapping para primer dashboard

---
+++ WebScrapping/WS_Manga.py
@@ -0,0 +1,108 @@
+import WS_Rank
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+def sacar_varios_datos(lista):
+    ln1 = list(lista[1].split(","))
+    lf = []
+    for l in ln1:
+        ln2 = list(l.strip().split("\n"))
+        if ln2[0] not in lf:
+            lf.append(ln2[0])
+    return lf
+
+def manga(rank_manga):
+    manga = {"Titulo": [], "Volumenes": [], "Capitulos": [], "Status": [], "Año": [], "Generos": [],"Temas":[],
+             "Demografia": [], "Editorial": [], "Autor": [], "Lectores": []}
+    for ra in (rank_manga["Link"]):
+        driver = ChromeDriverManager().install()
+        s = Service(driver)
+        opc = Options()
+        opc.add_argument("--window-size=1020,1200")
+        navegador = webdriver.Chrome(service=s, options=opc)
+        navegador.get(ra)
+        time.sleep(5)
+        soup = BeautifulSoup(navegador.page_source, "html.parser")
+        titulo = soup.find("span", attrs={"itemprop":"name"})
+        if titulo == None:
+            manga["Titulo"].append(None)
+        else:
+            manga["Titulo"].append(titulo.text)
+        info = soup.find_all("div", attrs={"class": "spaceit_pad"})
+        time.sleep(10)
+        lista_dic = [None, None, None, None, None, None, None, None, None, None]
+        for i in info:
+            lista=i.text.strip().split(":")
+            if lista[0]=="Volumes":
+                lista_dic[0]=lista[1]
+            elif lista[0]=="Chapters":
+                lista_dic[1]=lista[1]
+            elif lista[0]=="Status":
+                lista_dic[2]=lista[1]
+            elif lista[0]=="Published":
+                lista_dic[3]=lista[1]
+            elif lista[0] == "Genres":
+                dato=sacar_varios_datos(lista)
+                lista_dic[4]=dato
+            elif lista[0] == "Genre":
+                dato = sacar_varios_datos(lista)
+                lista_dic[4]=dato
+            elif lista[0] == "Theme":
+                dato=sacar_varios_datos(lista)
+                lista_dic[5]=dato
+            elif lista[0] == "Themes":
+                dato = sacar_varios_datos(lista)
+                lista_dic[5]=dato
+            elif lista[0] == "Demographic":
+                dato=sacar_varios_datos(lista)
+                lista_dic[6]=dato
+            elif lista[0] == "Demographics":
+                dato = sacar_varios_datos(lista)
+                lista_dic[6]=dato
+            elif lista[0] == "Serialization":
+                lista_dic[7] = lista[1]
+            elif lista[0]=="Authors":
+                lista_dic[8]=lista[1]
+            elif lista[0]=="Author":
+                lista_dic[8]=lista[1]
+            elif lista[0]=="Members":
+                lista_dic[9]=lista[1]
+            else:
+                continue
+        manga["Volumenes"].append(lista_dic[0])
+        manga["Capitulos"].append(lista_dic[1])
+        manga["Status"].append(lista_dic[2])
+        manga["Año"].append(lista_dic[3])
+        manga["Generos"].append(lista_dic[4])
+        manga["Temas"].append(lista_dic[5])
+        manga["Demografia"].append(lista_dic[6])
+        manga["Editorial"].append(lista_dic[7])
+        manga["Autor"].append(lista_dic[8])
+        manga["Lectores"].append(lista_dic[9])
+        lista_dic = [None, None, None, None, None, None, None, None, None, None]
+        navegador.close()
+    print(len(manga["Titulo"]))
+    print(len(manga["Volumenes"]))
+    print(len(manga["Capitulos"]))
+    print(len(manga["Status"]))
+    print(len(manga["Año"]))
+    print(len(manga["Generos"]))
+    print(len(manga["Temas"]))
+    print(len(manga["Demografia"]))
+    print(len(manga["Editorial"]))
+    print(len(manga["Autor"]))
+    print(len(manga["Lectores"]))
+    df = pd.DataFrame(manga)
+    df.to_csv("../DataSets/data_manga.csv")
+    print(df.tail())
+
+
+if __name__ == "__main__":
+    rank_manga=WS_Rank.web()
+    manga(rank_manga)
